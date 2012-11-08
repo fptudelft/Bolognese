5d851f8d33b3c3b2e6583eab206e056eee6c4196
Volker Lanting <volker@simlike.com>
2012-11-08 16:21:16 +0100

    We have monads!!!

--- src/main/scala/com/bolognese/OscaR.scala
+++ src/main/scala/com/bolognese/OscaR.scala
@@ -3,25 +3,42 @@ import oscar.cp.modeling.CPSolver
 import oscar.cp.core.CPVarInt
 import oscar.cp.modeling.sum
 
+// the monad with, fmap, join, and bind
+sealed abstract class OscaR[+A] extends Function0[A] {
+    def map[B](f:A=>B):OscaR[B] = {
+        OscaR.apply(f(this()))
+    }
+    
+    def flatten[B](a:OscaR[OscaR[B]]):OscaR[B] = {
+        OscaR.apply(a.apply().apply())
+    }
+    
+    def flatMap[B](f:A=>OscaR[B]):OscaR[B] = {
+        flatten(map(f))
+    }
+}
 
-
-
-class OscaR[A] {
-	var model : CPModel = null
-	
-	private def run : Collection[CPMFixedVar] = {
-	    val cp = new CPSolver;
-	    val varmap =  model.vars.map(oscarize(_,cp)).toMap
-	    var fixedMap = Map[String, Int]()
-	    oscarize(model.goal, cp, varmap) subjectTo 
-	    	model.constraints.foreach(oscarize(_,cp,varmap)) exploration {
-	    		cp.binaryFirstFail(varmap.values.toArray)
-	    		fixedMap = varmap.map(x=>(x._1, x._2.getValue))
-	    	}
-	    return fixedMap.map(t=>CPMFixedVar(t._1,t._2))
+// instance creator of OscaR monads, so this has return (apply)
+// and the more useful create function, which creates a meaningful OscaR[Collection[CPMFixedVar]] monad
+object OscaR {
+	def apply[A](e: => A):OscaR[A] = new OscaRImpl(e) 
+    def create(model:CPModel):OscaR[Collection[CPMFixedVar]] = {
+	    new OscaRImpl({
+	        val cp = new CPSolver
+	        val varmap =  model.vars.map(oscarize(_,cp)).toMap
+	        var fixedMap = Map[String, Int]()
+	        oscarize(model.goal, cp, varmap) subjectTo 
+	    		model.constraints.foreach(oscarize(_,cp,varmap)) exploration {
+	    			cp.binaryFirstFail(varmap.values.toArray)
+	    			fixedMap = varmap.map(x=>(x._1, x._2.getValue))
+	    		}
+	        fixedMap.map(t=>CPMFixedVar(t._1,t._2))
+	    })
 	}
-	
-	private def oscarize(g:AbstractCPMGoal, cp:CPSolver, vars:Map[String,CPVarInt]) : CPSolver = g match {
+    private class OscaRImpl[+A](e: =>A) extends OscaR[A] {
+    	def apply() = {println("Applied!");e}
+    }
+    private def oscarize(g:AbstractCPMGoal, cp:CPSolver, vars:Map[String,CPVarInt]) : CPSolver = g match {
 		case CPMMinimize(f) => cp.minimize(oscarize(f,cp,vars))
 	}
 	
@@ -47,4 +64,5 @@ class OscaR[A] {
     	case CPMIntCons(i) => CPVarInt(cp,i)
 	}
 }
+
 

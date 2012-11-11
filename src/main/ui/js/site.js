ee977eb5d4d6a5a68cbda6a7d2759dd22941030d
Joey Ezechiëls <joey.ezechiels@gmail.com>
2012-11-11 18:20:12 +0100

    Reformatted the layout of the UI code a little to make for easier reading

--- src/main/ui/js/site.js
+++ src/main/ui/js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  /*
-    CATEGORIES
+    /*
+      CATEGORIES
     */
     var categoryName = extractValueB('CategoryName');
     var categoryMin = extractValueB('CategoryMinPoints');
@@ -13,62 +13,71 @@ $(document).ready(function(){
     var categoryMinValidB = liftB(validNumber, categoryMin);
     var categoryMaxValidB = liftB(validNumber, categoryMax);
 
-    var categoryFormValidB = andB(categoryMinValidB, categoryMaxValidB,
-      categoryNameValidB);
+    var categoryFormValidB = andB(categoryMinValidB,
+                                  categoryMaxValidB,
+                                  categoryNameValidB);
 
     insertValueB(
-      ifB(categoryNameValidB, '#00FF00', '#FF0000'),
-      'CategoryName', 'style', 'border-color');
+        ifB(categoryNameValidB, '#00FF00', '#FF0000'),
+        'CategoryName', 'style', 'border-color');
 
     insertValueB(
-      ifB(categoryMinValidB, '#00FF00', '#FF0000'),
-      'CategoryMinPoints', 'style', 'border-color');
+        ifB(categoryMinValidB, '#00FF00', '#FF0000'),
+        'CategoryMinPoints', 'style', 'border-color');
 
     insertValueB(
-      ifB(categoryMaxValidB, '#00FF00', '#FF0000'),
-      'CategoryMaxPoints', 'style', 'border-color');
+        ifB(categoryMaxValidB, '#00FF00', '#FF0000'),
+        'CategoryMaxPoints', 'style', 'border-color');
 
     insertValueB(
-      ifB(categoryFormValidB, '#00FF00', '#FFFFFF'),
-      'CategoryInputTable', 'style', 'background-color');
+        ifB(categoryFormValidB, '#00FF00', '#FFFFFF'),
+        'CategoryInputTable', 'style', 'background-color');
 
     insertValueB(
-      ifB(categoryFormValidB, 'inline', 'none'),
-      'CategorySubmit', 'style', 'display');
+        ifB(categoryFormValidB, 'inline', 'none'),
+        'CategorySubmit', 'style', 'display');
 
     var clickedCategoriesE = extractEventE('CategorySubmit', 'click');
-  var close = constantB(-1); // The value was chosen because it is never used: Points can't be negative
+    var close = constantB(-1); // The value was chosen because it
+                               // is never used: Points can't be negative
 
-  // Serialize events
-  var categoryNameE = clickedCategoriesE.snapshotE(categoryName).delayE(1);
-  var categoryMinE = clickedCategoriesE.snapshotE(categoryMin).delayE(2);
-  var categoryMaxE = clickedCategoriesE.snapshotE(categoryMax).delayE(3);
-  var categoriesDelE = clickedCategoriesE.snapshotE(close).delayE(4);
+    // Serialize events
+    var categoryNameE = clickedCategoriesE.snapshotE(categoryName).delayE(1);
+    var categoryMinE = clickedCategoriesE.snapshotE(categoryMin).delayE(2);
+    var categoryMaxE = clickedCategoriesE.snapshotE(categoryMax).delayE(3);
+    var categoriesDelE = clickedCategoriesE.snapshotE(close).delayE(4);
 
-  var categoriesAllE = categoryNameE.mergeE(categoryMinE, categoryMaxE, categoriesDelE).delayE(5);
+    var categoriesAllE = categoryNameE.mergeE(categoryMinE,
+                                              categoryMaxE,
+                                              categoriesDelE).delayE(5);
 
-  var categoriesCollectedE = categoriesAllE.collectE([], function(item, rest) {
-      /*
-         collectE is not implemented in a way such that we
-         we could call a "second" and "third" on the rest and than
-         recurse with the "fourth" element. We resort to the
-         following rather ugly hack. Enjoy.
-         */
-         if(item === -1) {
-           var a = rest.pop();
-           var b = rest.pop();
-           var c = rest.pop();
-           rest.push(TR(c, b, a));
-         } else {
-          rest.push(TD(item));
+    var categoriesCollectedE = categoriesAllE.collectE([], function(item, rest) {
+        /*
+          collectE is not implemented in a way such that we
+          we could call a "second" and "third" on the rest and than
+          recurse with the "fourth" element. We resort to the
+          following rather ugly hack. Enjoy.
+        */
+        if(item === -1) {
+            var a = rest.pop();
+            var b = rest.pop();
+            var c = rest.pop();
+            rest.push(TR(c, b, a));
+        } else {
+            rest.push(TD(item));
         }
         return rest;
-      }).mapE(function(a) { return TABLE(TR(TH("Name"), TH("Minimum Points"), TH("Maximum Points")), a); });
+    }).mapE(function(a) {
+        return TABLE(TR( TH("Name"),
+                         TH("Minimum Points"),
+                         TH("Maximum Points") ),
+                     a);
+    });
 
-  insertDomE(categoriesCollectedE, 'CategoriesTable');
+    insertDomE(categoriesCollectedE, 'CategoriesTable');
 
-  /*
-    MODULES (Copy pasted....)
+    /*
+      MODULES (Copy pasted....)
     */
     var moduleName = extractValueB('ModulesName');
     var modulePoints = extractValueB('ModulesPoints');
@@ -78,56 +87,69 @@ $(document).ready(function(){
     var modulePointsValidB = liftB(validNumber, modulePoints);
     var moduleBookableCategoriesB = liftB(notEmpty, moduleBookableCategories);
 
-    var modulesFormValidB = andB(moduleNameValidB, modulePointsValidB,
-      moduleBookableCategories);
+    var modulesFormValidB = andB(moduleNameValidB,
+                                 modulePointsValidB,
+                                 moduleBookableCategories);
 
     insertValueB(
-      ifB(moduleNameValidB, '#00FF00', '#FF0000'),
-      'ModulesName', 'style', 'border-color');
+        ifB(moduleNameValidB, '#00FF00', '#FF0000'),
+        'ModulesName', 'style', 'border-color');
 
     insertValueB(
-      ifB(modulePointsValidB, '#00FF00', '#FF0000'),
-      'ModulesPoints', 'style', 'border-color');
+        ifB(modulePointsValidB, '#00FF00', '#FF0000'),
+        'ModulesPoints', 'style', 'border-color');
 
     insertValueB(
-      ifB(moduleBookableCategoriesB, '#00FF00', '#FF0000'),
-      'ModulesBookableCategories', 'style', 'border-color');
+        ifB(moduleBookableCategoriesB, '#00FF00', '#FF0000'),
+        'ModulesBookableCategories', 'style', 'border-color');
 
     insertValueB(
-      ifB(modulesFormValidB, '#00FF00', '#FFFFFF'),
-      'ModulesInputTable', 'style', 'background-color');
+        ifB(modulesFormValidB, '#00FF00', '#FFFFFF'),
+        'ModulesInputTable', 'style', 'background-color');
 
     insertValueB(
-      ifB(modulesFormValidB, 'inline', 'none'),
-      'ModulesSubmit', 'style', 'display');
+        ifB(modulesFormValidB, 'inline', 'none'),
+        'ModulesSubmit', 'style', 'display');
 
     var clickedModulesE = extractEventE('ModulesSubmit', 'click');
 
- // Serialize events
- var modulesNameE  = clickedModulesE.snapshotE(moduleName).delayE(1);
- var modulesPointsE = clickedModulesE.snapshotE(modulePoints).delayE(2);
- var modulesBookableCategoriesE = clickedModulesE.snapshotE(moduleBookableCategories).delayE(3);
- var modulesDelE = clickedModulesE.snapshotE(close).delayE(4);
+    // Serialize events
+    var modulesNameE  = clickedModulesE.snapshotE(moduleName).delayE(1);
+    var modulesPointsE = clickedModulesE.snapshotE(modulePoints).delayE(2);
+    var modulesBookableCategoriesE =
+        clickedModulesE.snapshotE(moduleBookableCategories).delayE(3);
+    var modulesDelE = clickedModulesE.snapshotE(close).delayE(4);
 
- var modulesAllE = modulesNameE.mergeE(modulesPointsE, modulesBookableCategoriesE, modulesDelE).delayE(5);
+    var modulesAllE = modulesNameE.mergeE(modulesPointsE,
+                                          modulesBookableCategoriesE,
+                                          modulesDelE).delayE(5);
 
- var modulesCollectedE = modulesAllE.collectE([], function(item, rest) {
-   if(item === -1) {
-     var a = rest.pop();
-     var b = rest.pop();
-     var c = rest.pop();
-     rest.push(TR(c, b, a));
-   } else {
-    rest.push(TD(item));
-  }
-  return rest;
-}).mapE(function(a) { return TABLE(TR(TH("Name"), TH("Points"), TH("Bookable Categories")), a); });
+    var modulesCollectedE = modulesAllE.collectE([], function(item, rest) {
+        if(item === -1) {
+            var a = rest.pop();
+            var b = rest.pop();
+            var c = rest.pop();
+            rest.push(TR(c, b, a));
+        } else {
+            rest.push(TD(item));
+        }
+        return rest;
+    }).mapE(function(a) {
+        return TABLE(TR( TH("Name"),
+                         TH("Points"),
+                         TH("Bookable Categories") ),
+                     a);
+    });
 
- insertDomE(modulesCollectedE, 'ModulesTable');
+    insertDomE(modulesCollectedE, 'ModulesTable');
 
- totalE = clickedModulesE.snapshotE(modulePoints).collectE(0, function(item, count) { return parseInt(item) + parseInt(count); });
+    totalE = clickedModulesE
+        .snapshotE(modulePoints)
+        .collectE(0, function(item, count) {
+            return parseInt(item) + parseInt(count);
+        });
 
-insertDomE(totalE, 'PointTotal');
+    insertDomE(totalE, 'PointTotal');
 
-  // Add sum of points!!
+    // Add sum of points!!
 });
